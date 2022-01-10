67b99f25c56e75f852ecd941e2620e29756aeb14
snaranjo <unheroicmail@gmail.com>
2022-01-10 16:19:08 -0500

    updating secrets

--- spotiPlus/spotify/secrets.py
+++ spotiPlus/spotify/secrets.py
@@ -1,4 +1,4 @@
 SPOTIFY_CLIENT_ID = "9ccb3fd472c14d95b4413275fd0db43a"
-SPOTIFY_CLIENT_SECRET = "75b7f5c03f394d1ca9a94803bb1a648a"
+SPOTIFY_CLIENT_SECRET = "c8b4780e2cba4854a1d821ab98350e06"
 REDIRECT_URI = "http://127.0.0.1:5000/"
 SCOPE = "user-library-read user-read-private user-read-email user-read-playback-state user-read-currently-playing user-top-read playlist-modify-public app-remote-control streaming"
